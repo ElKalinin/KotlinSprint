04cba9f6b341b764c9585ecae17f5de0992b9298
ElKalinin <i.kalinin@joinbet.com>
2024-09-19 21:06:51 +0300

    KS-2-3 refactor

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,13 +1,15 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
 
     val hourOfDeparture = 9
     val minuteOfDeparture = 39
-    val travelTimeInMinutes =457
+    val travelTimeInMinutes = 457
 
-    val hourOfArrival = (((hourOfDeparture * 60) + minuteOfDeparture) + travelTimeInMinutes) / 60
-    val minuteOfArrival = (((hourOfDeparture * 60) + minuteOfDeparture) + travelTimeInMinutes) % 60
+    val hourOfArrival = (((hourOfDeparture * MINUTES_IN_HOUR) + minuteOfDeparture) + travelTimeInMinutes) / MINUTES_IN_HOUR
+    val minuteOfArrival = (((hourOfDeparture * MINUTES_IN_HOUR) + minuteOfDeparture) + travelTimeInMinutes) % MINUTES_IN_HOUR
 
     print(hourOfArrival)
     print(':')
