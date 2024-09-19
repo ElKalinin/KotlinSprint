1154ba8757e6a3fc2312dd173070e20cf01d3c05
ElKalinin <i.kalinin@joinbet.com>
2024-09-19 21:44:27 +0300

    KS-2-5 refactor

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,10 +5,10 @@ import kotlin.math.pow
 fun main() {
 
     val downPayment = 70000
-    val annualPercentage = 0.167
+    val annualPercentage = 16.7
     val depositTerm = 20
 
-    val finalAmount = downPayment * (1 + annualPercentage).pow(depositTerm)
+    val finalAmount = downPayment * (1 + (annualPercentage / 100)).pow(depositTerm)
 
     println(String.format("%.3f" , finalAmount))
 
