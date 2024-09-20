bf057fc39a5bfef125babe8c654fea30b1a163c5
ElKalinin <i.kalinin@joinbet.com>
2024-09-20 11:37:11 +0300

    KS-3-2 change of surname

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val maidenName = "Андреева"
+    val marriedName = "Сидорова"
+    val ageBeforeMarriage: Int
+    val ageAfterMarriage: Int
+
+    ageBeforeMarriage = 20
+    ageAfterMarriage = 22
+
+    println("$maidenName $name $patronymic, $ageBeforeMarriage")
+    println("$marriedName $name $patronymic, $ageAfterMarriage")
+
+}
