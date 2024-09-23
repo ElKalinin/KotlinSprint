0644739c87b7322e51bbec96d68ac467e4a599b9
ElKalinin <i.kalinin@joinbet.com>
2024-09-23 11:54:01 +0300

    KS-4-1 refactor

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_4
 
+const val TABLES = 13
+
 fun main() {
 
-    val tables = 13
     val bookingToday = 13
     val bookingTomorrow = 9
 
-    println("Доступность столиков на сегодня: ${bookingToday < tables}")
-    println("Доступность столиков на завтра: ${bookingTomorrow < tables}")
+    println("Доступность столиков на сегодня: ${bookingToday < TABLES}")
+    println("Доступность столиков на завтра: ${bookingTomorrow < TABLES}")
 
 }
