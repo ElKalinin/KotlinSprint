8916fae5c960c9eea38a4bf2c22969e5614c57fd
ElKalinin <i.kalinin@joinbet.com>
2024-09-23 23:19:54 +0300

    KS-4-3 refactor

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4
 
+const val FAVORABLE_SUNNY_WEATHER = true
+const val FAVORABLE_OPEN_AWNING = true
+const val FAVORABLE_AIR_HUMIDITY = 20
+const val ADVERSE_TIME_OF_YEAR = "Зима"
+
 fun main() {
 
     var sunnyWeather: Boolean = true
@@ -7,6 +12,9 @@ fun main() {
     var airHumidity: Int = 20
     var timeOfYear: String = "Зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(sunnyWeather == true) && (openAwning == true) && (airHumidity == 20) && (timeOfYear != "Зима")}")
+    val favorableCondition =
+        (sunnyWeather == FAVORABLE_SUNNY_WEATHER) && (openAwning == FAVORABLE_OPEN_AWNING) && (airHumidity == FAVORABLE_AIR_HUMIDITY) && (timeOfYear != ADVERSE_TIME_OF_YEAR)
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableCondition")
 
 }
