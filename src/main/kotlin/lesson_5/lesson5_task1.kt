e72f3e7d9dc9d28215047871d1ec8dde559821e1
ElKalinin <i.kalinin@joinbet.com>
2024-09-23 23:30:13 +0300

    KS-5-1 refactor

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -2,10 +2,14 @@ package org.example.lesson_5
 
 fun main() {
 
-    println("Введите правильный ответ на 2+2:")
+    val verificationNumber1 = 2
+    val verificationNumber2 = 3
+    val verificationAddition = verificationNumber1 + verificationNumber2
+
+    println("Введите правильный ответ на $verificationNumber1+$verificationNumber2:")
     val answer = readln().toInt()
 
-    if (answer == 4) println("Добро пожаловать!")
+    if (answer == verificationAddition) println("Добро пожаловать!")
     else println("Доступ запрещён.")
 
 }
