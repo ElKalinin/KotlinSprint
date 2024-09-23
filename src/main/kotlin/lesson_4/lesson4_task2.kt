b59a4a80074f361b01d3613a35bc65f067817729
ElKalinin <i.kalinin@joinbet.com>
2024-09-23 23:03:00 +0300

    KS-4-2 refactor

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -12,7 +12,12 @@ fun main() {
     val weightCargo2 = 50
     val volumeCargo2 = 100
 
-    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average': ${(weightCargo1 > MIN_WEIGHT_AVERAGE) && (weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)}")
-    println("Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average': ${(weightCargo2 > MIN_WEIGHT_AVERAGE) && (weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)}")
+    val comparisonResultCargo1 =
+        (weightCargo1 > MIN_WEIGHT_AVERAGE) && (weightCargo1 <= MAX_WEIGHT_AVERAGE) && (volumeCargo1 < MAX_VOLUME_AVERAGE)
+    val comparisonResultCargo2 =
+        (weightCargo2 > MIN_WEIGHT_AVERAGE) && (weightCargo2 <= MAX_WEIGHT_AVERAGE) && (volumeCargo2 < MAX_VOLUME_AVERAGE)
+
+    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average': $comparisonResultCargo1")
+    println("Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average': $comparisonResultCargo2")
 
 }
