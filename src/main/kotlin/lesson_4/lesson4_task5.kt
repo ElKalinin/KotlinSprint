0e554f9285bcf94d87ca8fa53699e7c10b2e3327
ElKalinin <i.kalinin@joinbet.com>
2024-09-24 13:53:10 +0300

    KS-4-5 refactor

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_4
 
+const val MIN_CREW = 55
+const val MAX_CREW = 70
+const val MIN_PROVISIONS = 50
+const val IS_DAMAGE = true
+const val IS_FAVORABLE_WEATHER = true
+
 fun main() {
 
     var damage: Boolean
@@ -8,18 +14,22 @@ fun main() {
     var weather: Boolean
 
     println("Введите информацию о повреждении (true/false):")
-    damage = readLine()!!.toBoolean()
+    damage = readln().toBoolean()
 
     println("Введите информацию о количестве экипажа:")
-    crew = readLine()!!.toInt()
+    crew = readln().toInt()
 
     println("Введите информацию о количестве ящиков с провизией:")
-    provisions = readLine()!!.toInt()
+    provisions = readln().toInt()
 
     println("Введите информацию о благоприятности метеоусловий (true/false):")
-    weather = readLine()!!.toBoolean()
+    weather = readln().toBoolean()
 
-    val flightClearance: Boolean = ((damage == false) && (crew >= 55) && (crew <= 70) && (provisions > 50) && ((weather == true) || (weather == false))) || ((damage == true) && (crew == 70) && (weather == true) && (provisions >= 50))
+    val flightClearance: Boolean =
+        ((damage != IS_DAMAGE) && (crew >= MIN_CREW) && (crew <= MAX_CREW) && (provisions > MIN_PROVISIONS) &&
+                ((weather == IS_FAVORABLE_WEATHER) || (weather != IS_FAVORABLE_WEATHER))) ||
+                ((damage == IS_DAMAGE) && (crew == MAX_CREW) &&
+                        (weather == IS_FAVORABLE_WEATHER) && (provisions >= MIN_PROVISIONS))
 
     println("Корабль готов к полёту: $flightClearance")
 
